11811628c2ee8672c26ed2ea62f9dfc5fcf046e3
LasCazas <132779159+LasCazas@users.noreply.github.com>
2023-09-16 15:14:26 -0300

    Update Constante.c

--- funcionou/Constante.c
+++ funcionou/Constante.c
@@ -1,8 +1,8 @@
 #line 1 "C:\\Users\\LAIC\\Documents\\Arduino\\PIDSimples\\Constante.c"
 //Declaração de Constantes
 #define DELTAPULSO 11 //Pesquisei e deu 11 pulsos por volta
-#define PWME 80
-#define PWMD 76
+#define PWME 100
+#define PWMD 95
 const int Branco = 1;
 const int Preto = 0;
 #define Kp 15 //Variavel que multiplica o erro da posicao do carro para o ajuste fino
@@ -11,21 +11,21 @@ const int Preto = 0;
 const int buttonPin = 2;  // Pino do botão
 
 // Define os pinos dos sensores E,C,D,O,Esq,Centro,Direita,Oposto
-#define PSenOE 26
-#define PSenE 27
-#define PSenC 28
-#define PSenD 29
-#define PSenOD 30
-//Pinos utilizados na montagem no Arduino
-#define PEnCEsq 2 //Pino Encoder Esquerdo
-#define PEnCDir 3 //Pino Encoder Direito
-
-#define ENA 5 //Esquerda  //D1
+#define PSenOE 10
+#define PSenE A5
+#define PSenCE A4
+#define PSenCD A3
+#define PSenD A2
+#define PSenOD 11
+#define IR 8
+#define corte 1020
+//MOTORES
+#define ENA 3 //Esquerda  //D1
 #define IN1 4  //Esquerda //D2
-#define IN2 0  //Esquerda //D3
-#define IN3 2  //Direita  //D4
-#define IN4 14  //Direita //D5
-#define ENB 12 //Direita  //D6
+#define IN2 5  //Esquerda //D3
+#define IN3 6  //Direita  //D4
+#define IN4 7 //Direita //D5
+#define ENB 9 //Direita  //D6
 #define Parar 0
 #define Frente 1
 #define Direita 2
